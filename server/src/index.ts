bf2c251487008df3e34fcc205bb4928c5a64ccd8
Leandro Buzzi <77006961+likahon@users.noreply.github.com>
2025-11-28 18:13:56 -0300

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
   
-  app.get('*', (req, res) => {
+  app.get('*', (req: any, res: any) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 }
